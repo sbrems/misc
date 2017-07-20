3a2c5149d0c279f2a757a5a2d97d70f3955e0a66
SBREMS <sbrems@lsw.uni-heidelberg.de>
2017-07-20 17:18:47 +0200

    added starclass functionality to fit BB to filterbands and then extrapolate to another band

---
+++ fit_blackbody.py
@@ -0,0 +1,162 @@
+import numpy as np
+from astropy.modeling.blackbody import blackbody_lambda as bbl
+import astropy.units as u
+from scipy import integrate
+from scipy.optimize import least_squares
+from astropy.table import Table
+import os
+import pickle
+
+
+# give some properties
+_pnfilt = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                       'VOSA_filtercurves')  # pn for filters
+
+
+def fit_temp2flux(fluxes, filtnames, nusepoints=15):
+    '''Give lists for fluxes, fluxerrs and filtnames for the points
+    you want to fit a blackbody to. It returns the temperature in K'''
+    # first get the integrated fluxes of the magnitude bands
+    fluxints, fluxerrs = [], []
+    for ii in range(len(filtnames)):
+        tfilt = get_filter(filtnames[ii])    # make the points where to
+        # evaluate the function. It uses the closest
+        # filterpoints afterwards
+        usepoints = np.linspace(np.min(tfilt['lambda'].to('Angstrom')),
+                                np.max(tfilt['lambda'].to('Angstrom')),
+                                num=nusepoints)
+        new_throughput = np.full(nusepoints, np.nan)
+        for ipoint in range(nusepoints):
+            new_throughput[ipoint] = tfilt['transmis'][
+                find_nearest(tfilt['lambda'],
+                             usepoints[ipoint])]
+        fluxints.append(integrate_flux(fluxes[0][ii] * new_throughput,
+                                       usepoints))
+        fluxerrs.append(integrate_flux(fluxes[1][ii] * new_throughput,
+                                       usepoints))
+    fluxints = fluxints * fluxints[0].unit
+    fluxerrs = fluxerrs * fluxerrs[0].unit
+    fitres = least_squares(residuals, [2000, 1e-17],
+                           args=[[fluxints, fluxerrs, filtnames,
+                                  nusepoints]],
+# use bounds for method trm # bounds=[[0, 1e-40], [20000, 1e-5]],
+                           method='lm')
+    fittemparea = fitres.x
+    return fittemparea
+
+
+def residuals(Temparea, fluxfilts):
+    '''The function which is being minimized: BB-flux and obs flux'''
+    fluxes, fluxerrs, filtnames, nusepoints = fluxfilts
+    skyarea = Temparea[1]*u.sr
+    Temp = Temparea[0]*u.K
+    bbfluxes = [skyarea * filtered_flux(Temp, filtname,
+                                        nusepoints=nusepoints) for
+                filtname in filtnames]
+    bbfluxes *= bbfluxes[-1].unit
+
+    resid = (bbfluxes - fluxes) / fluxerrs
+    # print('Trying Temp = {}, skyarea = {}, resid = {}'.format(
+    #    Temp, skyarea, resid))
+    # print(bbfluxes, fluxes, bbfluxes-fluxes,resid, sep='\n')
+    return resid
+
+
+def filtered_flux(Temp, filtname, nusepoints=15):
+    '''Return the integrated flux of the filter mutliplied with the
+    transmission.'''
+    tfilt = get_filter(filtname)
+    # now integrate with the nearest transmission point
+    vecbb_w_filter = np.vectorize(bb_w_filter, excluded=[1, 2])
+    # make the points where to evaluate the function. It uses the closest
+    # filterpoints afterwards
+    usepoints = np.linspace(np.min(tfilt['lambda'].to('Angstrom')),
+                            np.max(tfilt['lambda'].to('Angstrom')),
+                            num=nusepoints)
+    fluxdenses = vecbb_w_filter(usepoints.value,
+                                Temp.to('K').value, tfilt)
+    fluxdenses = fluxdenses * u.erg / u.cm**2 / u.s / u.Angstrom / u.sr
+    flux = integrate_flux(fluxdenses, usepoints)
+
+    return flux
+
+
+def integrate_flux(fluxdenses, xpoints):
+    '''Integrate the flux of a source. If there is only one value given,
+    assume a constant fluxdensity.'''
+    flux = integrate.trapz(fluxdenses,
+                           x=xpoints)
+
+    return flux
+
+
+def get_filter(filtname):
+    '''Reads and returns Astropy table with the filter transmission'''
+    pfnfilt = os.path.join(_pnfilt,
+                           filtname.replace('/','_') + '.dat')
+
+    tfilt = Table(np.loadtxt(pfnfilt), names=['lambda', 'transmis'])
+    tfilt['lambda'].unit = u.Angstrom
+
+    return tfilt
+
+
+def bb_w_filter(wavel, Temp, tfilt):
+    flux = bbl(wavel * u.Angstrom, Temp) *\
+           tfilt['transmis'][find_nearest(tfilt['lambda'],
+                                          wavel)]
+    return flux.value
+
+
+def find_nearest(array, value):
+    idx = (np.abs(array-value)).argmin()
+    return idx
+
+
+def loadfiltzeros(filtname):
+    '''Load zero filters. If not existent ask for it and add it'''
+    pfnfilt = os.path.join(_pnfilt, 'filter_zeropoints.p')
+    if os.path.exists(pfnfilt):
+        filterzeros = pickle.load(open(pfnfilt, 'rb'))
+    else:
+        filterzeros = {}
+
+    if filtname in filterzeros.keys():
+        zeroflux = filterzeros[filtname]
+    elif filtname == 'all':
+        zeroflux = filterzeros
+    else:
+        zeroflux = np.float(eval(input('Please give the Zero point of filter "{}"\
+ in the Vega system in (erg/cm2/s/A)'.format(filtname))))
+        zeroflux = zeroflux * u.erg / u.cm**2 / u.s / u.Angstrom
+        filterzeros[filtname] = zeroflux
+        pickle.dump(filterzeros, open(pfnfilt, 'wb'))
+    return zeroflux
+
+
+def mag2flux(mag, filtname):
+    '''Load the zero magnitude from the dict file and convert it to
+    flux'''
+    zeroflux = loadfiltzeros(filtname)
+    flux = zeroflux / 10**(mag/2.5)
+    return flux
+
+
+def flux2mag(flux, filtname, nusepoints=15):
+    '''Load the zero magnitude from the dict file and convert it to
+    flux'''
+    zeroflux = loadfiltzeros(filtname)
+    tfilt = get_filter(filtname)
+    usepoints = np.linspace(np.min(tfilt['lambda'].to('Angstrom')),
+                            np.max(tfilt['lambda'].to('Angstrom')),
+                            num=nusepoints)
+    new_throughput = np.full(nusepoints, np.nan)
+    for ipoint in range(nusepoints):
+        new_throughput[ipoint] = tfilt['transmis'][
+            find_nearest(tfilt['lambda'],
+                         usepoints[ipoint])]
+        zerofluxint = integrate_flux(zeroflux * new_throughput,
+                                 usepoints)
+
+    mag = u.Magnitude(-2.5 * np.log10(flux/zerofluxint))
+    return mag
--- starclass.py
+++ starclass.py
@@ -4,89 +4,148 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 
 from spt_magnitude_conversion import split_spt
+import fit_blackbody
+from fit_blackbody import fit_temp2flux, flux2mag, mag2flux
 
-class Star:
-        '''Save properties of a star. So far available:
-        aliases (auto from Simbad)
-        Coordinates (=['auto',] for Simbad query),
-        coord_units: default: ['u.hourangle','u.deg'],
-        default coord_frame ='icrs'
-        Magnitudes (with the filterband, default filterband = None)
-        SpT (also num)
-        aliases
-        PM
-        parallax
-        '''
-        _kind = 'star'
+
+class Star():
+    '''Save properties of a star. So far available:
+    aliases (auto from Simbad)
+    Coordinates (=['auto',] for Simbad query),
+    coord_units: default: ['u.hourangle','u.deg'],
+    default coord_frame ='icrs'
+    Magnitudes (with the filterband, default filterband = None)
+    SpT (also num)
+    aliases
+    PM
+    Magnitude bands
+    Temperature detemination via a BB fit to these bands
+    parallax
+    '''
+    def __init__(self, name,
+                 coordinates=[None, [u.hourangle, u.deg], 'icrs'],
+                 aliases=None,
+                 mag=None, filterband=None, magerror=None,
+                 SpT=None, SpC=None, temperature=None):
+        self.name = name
+        self.SpT = SpT
+        self.temperature = temperature
+        self.skyarea = None #the result when fitting a temperature
         
-        def __init__(self, name,
-                     coordinates=[None,[u.hourangle, u.deg],'icrs'],
-                     aliases = None,
-                     mag = np.array([None,]), filterband = np.array([None,]),
-                     SpT=None, SpC=None):
-           self.name = name
-           self.SpT          = SpT
+        self._aliases = aliases
+        self._coordinates = coordinates
+        self._mag = mag
+        self._filterband = filterband
+        self._magerror = magerror
+    _kind = 'star'
 
-           self._aliases = aliases
-           self._coordinates = coordinates
-           self._mag         = mag
-           self._filterband  = filterband
+    def SpT_num(self):
+        print('Blubb')
+        return np.sum(split_spt(self.SpT)[0:2])
 
-        @property
-        def coordinates(self):
-            return self._coordinates
+    def SpC_num(self):
+        return split_spt(self.SpT)[2]
 
-        @coordinates.setter
-        def coordinates(self,coords):
-            if coords[0] == 'auto' or coords =='auto':
-                coords = Simbad.query_object(self.name)['RA','DEC']
-                coords = [coords['RA'][0]+' '+coords['DEC'][0],]
-                self._coordinates = SkyCoord(coords, unit=(u.hourangle,u.deg),frame='icrs')
-            else:
-                assert not isinstance(coords, basestring) #make sure it is a list
-                if len(coords) not in [1,2,3]:
-                    raise ValueError('Coordinates not understood. Set first to auto or provide\
+    def flux(self):
+        fluxes = []
+        fluxerrs = []
+        for mm, msig, fb in zip(self._mag, self._magerror, self._filterband):
+            fluxes.append(  mag2flux(mm, fb))
+            fluxerrs.append(mag2flux(mm + msig, fb))
+        fluxes = fluxes * fluxes[-1].unit
+        fluxerrs = fluxerrs * fluxerrs[-1].unit
+        return fluxes, fluxerrs
+
+    def get_temp_via_bb(self, nusepoints=15):
+        '''Fit a bb to the filtercurves transmission given
+        in slef.mag nusepoints gives the number of points used
+        in the filtercurves. The second value gives the scaling of 
+        the flux, e.g. sth similar to the steradian.'''
+        print('Getting the temperature of {} using wavebands {}\n\
+This may take some time.'.format(
+                self.name, self._filterband))
+        return fit_temp2flux(self.flux(), self._filterband,
+                             nusepoints=nusepoints)
+
+    def temp2mag_from_filter(self, filterband, nusepoints=15, temperature=None,
+                             skyarea = None):
+        '''Get The magnitude in a filterband fitting a BB using the found
+        temp and skyarea. Or give a temp and skyarea'''
+        if temperature is None:
+                temperature = self.temperature
+        if skyarea is None:
+                skyarea = self.skyarea
+        flux = self.skyarea * fit_blackbody.filtered_flux(temperature,
+                                                          filterband,
+                                                          nusepoints=nusepoints)
+        return flux2mag(flux, filterband, nusepoints=nusepoints)
+
+    def show_filterzeros(self):
+        return fit_blackbody.loadfiltzeros('all')
+
+    @property
+    def coordinates(self):
+        return self._coordinates
+
+    @coordinates.setter
+    def coordinates(self, coords):
+        if 'auto' in coords:
+            coords = Simbad.query_object(self.name)['RA', 'DEC']
+            coords = [coords['RA'][0]+' '+coords['DEC'][0], ]
+            self._coordinates = SkyCoord(coords, unit=(u.hourangle, u.deg),
+                                             frame='icrs')
+        else:
+            # make sure it is a list
+            if len(coords) not in [1, 2, 3]:
+                raise ValueError('Coordinates not understood. Set first to auto or provide\
 them in a tuple with \
 first: ra,dec, second coord_units, third: coord_frame. Two and three are optional.\
 E.g. [["2:00:00 -1:00:00"],[u.hourangle, u.deg],"icrs"]')
-                crds = coords[0]
-                coord_units = self._coord_units
-                coord_frome = self._coord_frame
-                if len(coords) >= 2:
-                    coord_units = coords[1]
-                elif len(coords) ==3:
-                    coord_frame = coords[2]
-                    
-                self._coordinates = SkyCoord(coords, unit=coord_units,frame = coord_frame)
-                
-        @property
-        def mag(self):
-            return self._mag
-        
-        @mag.setter
-        def mag(self,value,filterband=[None,]):
-            self._mag =       np.hstack((self._mag,np.array(value)))
-            self._filterband= np.hstack((self._filterband,np.array(filterband)))
+            crds = coords[0]
+            coord_units = self._coord_units
+            coord_frame = self._coord_frame
+            if len(coords) >= 2:
+                coord_units = coords[1]
+            elif len(coords) == 3:
+                coord_frame = coords[2]
 
-        @mag.deleter
-        def mag(self,value):
-            indices = np.where(self._mag == value)
-            print('Removing entries nr {} from mag and filterband'.format(indices))
-            self._mag        = np.delete(self._mag,indices,axis=0)
-            self._filterband = np.delete(self._filterband,indices,axis=0)
-        
-        @property
-        def SpT_num(self):
-            return np.sum(split_spt(self.SpT)[0:2])
+        self._coordinates = SkyCoord(coords, unit=coord_units,
+                                     frame=coord_frame)
 
-        @property
-        def SpC_num(self):
-            return split_spt(self.SpT)[2]
+    @property
+    def mag(self):
+        return [self._mag, self._magerror, self._filterband]
 
-        @property
-        def aliases(self):
-            if self._aliases == None:
-                self._aliases = list(Simbad.query_objectids(self.name)['ID'])
-            return self._aliases
+    @mag.setter
+    def mag(self, value_filt):
+        value_filt = np.array(value_filt)
+        if (value_filt.shape[-1] != 3) or\
+           (len(value_filt.shape) > 3):
+            raise ValueError('Please give a filter name and error to the magnitude\
+            value. E.g. star.mag = [[3, 1, "HST_ACS_WFC.F814W_77"]]')
+        if self.mag[0] is None:
+            self._mag = np.array(value_filt[:, 0], dtype=np.float)
+            self._magerror   = np.array(value_filt[:, 1], dtype=np.float)
+            self._filterband = np.array(value_filt[:, 2])
+        else:
+            self._mag = np.hstack((self._mag, np.array(value_filt[:, 0],
+                                                       dtype=np.float)))
+            self._magerror = np.hstack((self._magerror,
+                                        np.array(value_filt[:, 1],
+                                                 dtype=np.float)))
+            self._filterband = np.hstack((self._filterband,
+                                          np.array(value_filt[:, 2])))
+    @mag.deleter
+    def mag(self):
+        print('Removing mags, errors and filterbands for {}'.format(self.name))
+        self._mag        = None
+        self._filterband = None
+        self._magerror   = None
+
+    @property
+    def aliases(self):
+        if self._aliases is None:
+            self._aliases = list(Simbad.query_objectids(self.name)['ID'])
+        return self._aliases
+
             
-
